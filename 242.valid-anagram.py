c60caba2596b281cabe4646dd505da76b78e82cb
Spencer Fisher <spencer.fisher@billgo.com>
2023-11-10 00:39:44 -0800

    quick anagram didnt speed it up much

--- 242.valid-anagram.py
+++ 242.valid-anagram.py
@@ -1,3 +1,8 @@
+# @before-stub-for-debug-begin
+from python3problem242 import *
+from typing import *
+# @before-stub-for-debug-end
+
 #
 # @lc app=leetcode id=242 lang=python3
 #
@@ -8,8 +13,19 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         # basically count the letters and make sure each one has the same.
-        return (len(s) == len(t)) and (self.buildDict(s) == self.buildDict(t))
+        return self.isQuickAnagram(s, t) and (self.buildDict(s) == self.buildDict(t))
 
+    # a quick sanity check (not conclusive (probably))
+    def isQuickAnagram(self, s: str, t:str) -> bool:
+        return (len(s) == len(t)) and (self.calcAscii(s) == self.calcAscii(t))
+        
+        
+    def calcAscii(self, s:str) -> int:
+        i=0
+        for c in s:
+            i+=ord(c)
+        return i
+    
     def buildDict(self, s: str) -> dict:
         t = {}
         for c in s:
