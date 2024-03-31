8abf725024d05f82abce3ac41bd5e60f46520731
Spencer Fisher <spencer.fisher@billgo.com>
2024-03-31 16:46:34 -0700

    figured it out

--- 1705.maximum-number-of-eaten-apples.py
+++ 1705.maximum-number-of-eaten-apples.py
@@ -5,6 +5,8 @@
 #
 
 # @lc code=start
+from typing import List
+
 class Solution:
     def eatenApples(self, apples: List[int], days: List[int]) -> int:
         # first lets create a list of all the expiration dates. 
@@ -13,45 +15,63 @@ class Solution:
         # then walk through and see how many apples we can eat before all apples expire.
 
         appleCounts = {}
-        finalDatedApples = 0
-        for day in range(len(apples)):
-            # check if we have apples on this day
-            if (apples[day] > 0):
-                # calculate these apples expiration date
-                expirationDate = day + days[day]
-
-                # check if we have other apples on this date
-                if expirationDate in appleCounts.keys():
-                    appleCounts[expirationDate] += apples[day]
-                else:
-                    appleCounts[expirationDate] = apples[day]
-
-        day = 0
+        finalExpiration = 0
+        dayIndex = 0
         applesEaten = 0
-        # we need to keep track of which apples we are currently eating.
-        # this value cannot be less than the current day counter.
-        applesDated = 0 
-        while len(appleCounts.keys()) > 0:
-            if applesDated < day:
-                applesDated += 1
-                continue
-            # make sure there are some apples to eat here
-            if day in appleCounts.keys() and appleCounts[day] > 0:
-                # found apples to eat. yay
-                day += 1
-                applesEaten += 1
+        applesDated = 0
 
-                # if its the last apple or the apples will expire lets remove them
-                if appleCounts[applesDated] == 1 or day == applesDated:
-                    del appleCounts[applesDated]
-                    applesDated += 1
-                    continue
-                
-                appleCounts[applesDated] -= 1
-            else:
-                # we ate all the apples from that expiration. Lets move along.
+        while dayIndex < len(apples) or dayIndex < finalExpiration:
+            # if its a day we could generate apples lets check that
+            if dayIndex < len(apples):
+                # check if we have apples on this day
+                if (apples[dayIndex] > 0):
+                    # calculate these apples expiration date
+                    expirationDate = dayIndex + days[dayIndex]
+
+                    # check if these expire before some other apples we're currently eating
+                    if expirationDate < applesDated:
+                        applesDated = expirationDate
+                        
+                    # record final apple expiration date
+                    if expirationDate > finalExpiration:
+                        finalExpiration = expirationDate
+
+                    # check if we have other apples on this date
+                    if expirationDate in appleCounts.keys():
+                        appleCounts[expirationDate] += apples[dayIndex]
+                    else:
+                        appleCounts[expirationDate] = apples[dayIndex]
+                        
+            # start walkthrough of eating apples. If we want to calculate all dates then walk
+            # we won't know when the apples are created. If we do it at the same time we know
+            # thats the apples will exist
+                        
+            # days start at 1 not 0            
+            day = dayIndex + 1
+            if applesDated < day:
+                applesDated = day
+            
+            # look for some good apples
+            while applesDated not in appleCounts.keys() or appleCounts[applesDated] == 0:
+                if applesDated > finalExpiration:
+                    break
                 applesDated += 1
+
+            if applesDated <= finalExpiration:
+                # found apples to eat. yay
+                applesEaten += 1
+                appleCounts[applesDated] -= 1
+
+            # cant slow the inevitable passage of time
+            dayIndex += 1
+
         return applesEaten
+    
+    
+# if __name__ == '__main__':
+#     apples = [1,2,3,5,2]
+#     days = [3,2,1,4,2]
+#     Solution.eatenApples(None, apples, days)
         
 # @lc code=end
 
