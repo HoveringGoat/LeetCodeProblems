f90fa4d53419c516fee472ba8f4bf0499922af91
Spencer Fisher <spencer.fisher@billgo.com>
2024-04-01 19:28:48 -0700

    still too slow

--- 87.scramble-string.py
+++ 87.scramble-string.py
@@ -11,57 +11,55 @@ from typing import *
 
 # @lc code=start
 class Solution:
-    masterScrambles = {} 
+    masterAnagrams = {} 
+    masterScrambles = {}
+
     def isScramble(self, s1: str, s2: str) -> bool:
 
         # couple trivial cases
+        if len(s1) != len(s2):
+            return False
         if s1 == s2:
             return True
         if s1[::-1] == s2:
             return True
         
-        def scramble(self, substring: str, restStringLeft: str, restStringRight: str) -> bool:
+        if (s1+","+s2) in self.masterScrambles:
+            return True
+        
+        print(len(s1))
+        
+        def scramble(self, substring: str, index:int) -> bool:
             # check if we have found it
-            if len(substring) == 1:
-                a = s2[len(restStringLeft)]
-                return a == substring
-
+            l = index + len(substring)
+            a = s2[index:l]
+            if a == substring:
+                return True
+            elif len(substring) == 1:
+                return False
             
-            # ensure the rest of string right matches
-            if len(restStringRight) > 0:
-                a = s2[-len(restStringRight):]
-                if Solution.isScramble(self, a, restStringRight) == False:
-                    return False
-                
-            # ensure the rest of string left matches
-            if len(restStringLeft) > 0:
-                a = s2[:len(restStringLeft)]
-                if Solution.isScramble(self, a, restStringLeft) == False:
-                    return False
-                
             # check possible substring permutations
             for i in range(1, len(substring), 1):
                 sub1 = substring[:i] # left substring
                 sub2 = substring[i:] # right substring
-                foundSub1 = False
-                foundSub2 = False
-
-                # no flip check further permutation in left substring
-                if scramble(self, sub1, restStringLeft, sub2+restStringRight):
-                    foundSub1 = True
-                # flip check further permutation in left substring
-                elif scramble(self, sub1, restStringLeft+sub2, restStringRight):
-                    foundSub1 = True
-                
-                # no flip check further permutation in right substring
-                if scramble(self, sub2, restStringLeft+sub1, restStringRight):
-                    foundSub2 = True
-                # flip check further permutation in right substring
-                elif scramble(self, sub2, restStringLeft, sub1+restStringRight):
-                    foundSub2 = True
+                l = index + len(sub1)
+                a = s2[index:l]
+                # check if left half is a scramble of the same portion of s2
+                if Solution.isScramble(self, a, sub1):
+                    a = s2[l:l+len(sub2)]
+                    # check if right half is a scramble of the same portion of s2
+                    if Solution.isScramble(self, a, sub2):
+                        return True
                     
-                if foundSub1 and foundSub2:
-                    return True
+                    
+                l = index + len(sub2)
+                a = s2[index:l]
+                # check if left half is a scramble of the opposite portion of s2
+                if Solution.isScramble(self, a, sub2):
+                    a = s2[l:l+len(sub1)]
+                    # check if right half is a scramble of the opposite portion of s2
+                    if Solution.isScramble(self, a, sub1):
+                        return True
                 
             return False
         
@@ -71,32 +69,32 @@ class Solution:
         dict1 = {}
         dict2 = {}
 
-        if s1 in self.masterScrambles:
-            dict1 = self.masterScrambles[s1]
+        if s1 in self.masterAnagrams:
+            dict1 = self.masterAnagrams[s1]
         else:
             for i in s1:
                 if i in dict1:
                     dict1[i] += 1
                 else:
                     dict1[i] = 1
-            self.masterScrambles[s1] = dict1
+            self.masterAnagrams[s1] = dict1
         
-        if s2 in self.masterScrambles:
-            dict2 = self.masterScrambles[s2]
+        if s2 in self.masterAnagrams:
+            dict2 = self.masterAnagrams[s2]
         else:
             for i in s2:
                 if i in dict2:
                     dict2[i] += 1
                 else:
                     dict2[i] = 1
-            self.masterScrambles[s2] = dict2
+            self.masterAnagrams[s2] = dict2
         
         if dict1 != dict2:
             return False
         
         # trim em before doing the brute.
         # we cannot assume the bits trimmed are not moved around
-        
+
         # i = 0
         # while i < len(s1)-1:
         #     if s1[i] == s2[i]:
@@ -132,7 +130,12 @@ class Solution:
         # we can compare them and see if theyre a match. If we ever find a match we can
         # return true
 
-        return scramble(self, s1, "", "")
+        result = scramble(self, s1, 0)
+        if len(s1) == 27:
+            a = "neet"
+        if result:
+            self.masterScrambles[(s1+","+s2)] = True
+        return result
 
 
         
