7bc0617989115709bd75ff1a579a04955ff41f76
Spencer Fisher <spencer.fisher@billgo.com>
2024-04-18 18:13:14 -0700

    sort solution

--- 217.contains-duplicate.py
+++ 217.contains-duplicate.py
@@ -7,12 +7,22 @@
 # @lc code=start
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        # create set
+        # set solution
         numSet = set()
         for i in nums:
             if i in numSet:
                 return True
             numSet.add(i)
+        return False
+    
+        # sort solution
+        nums.sort()
+        last = None
+        for i in nums:
+            if last == i:
+                return True
+            last = i
+
         return False
         
 # @lc code=end
