7bdbd57f7bd62a53f64f107428d691d3d33f709f
Spencer Fisher <spencer.fisher@billgo.com>
2024-04-25 14:27:54 -0700

    Update 42.trapping-rain-water.py
    ten min solve

--- 42.trapping-rain-water.py
+++ 42.trapping-rain-water.py
@@ -7,6 +7,31 @@
 # @lc code=start
 class Solution:
     def trap(self, height: List[int]) -> int:
+        lastValue = 0
+        lastValueIndex = 0
+        lastValueWater = 0
+        water = 0
+        for index, value in enumerate(height):
+            waterTrapped = lastValue - value
+            if waterTrapped <= 0:
+                lastValue = value
+                lastValueIndex = index
+                lastValueWater = water
+            else:
+                water += waterTrapped
+        
+        # discard everything after the last "max" height
+        # reverse array and count water from the other side
+        water = lastValueWater
+        lastValue = 0
+        for index in range(len(height)-1, lastValueIndex, -1):
+            waterTrapped = lastValue - height[index]
+            if waterTrapped <= 0:
+                lastValue = height[index]
+            else:
+                water += waterTrapped
+
+        return water
         
 # @lc code=end
 
