7a585129ab4bd10048e349a48ede41e34abe9219
Spencer Fisher <spencer.fisher@billgo.com>
2024-05-06 14:38:28 -0700

    Update 345.reverse-vowels-of-a-string.py

--- 345.reverse-vowels-of-a-string.py
+++ 345.reverse-vowels-of-a-string.py
@@ -7,19 +7,61 @@
 # @lc code=start
 class Solution:
     def reverseVowels(self, s: str) -> str:
-        reversedVowels = ""
+        # v2 keep two pointers and start at the start and end of the string
+        # move them each in until they encounter a vowel.
+        # if leftindex < rightindex swap the chars
+        
+        # both solutions are O(n) but this one should be moderately faster
+
+        # this one probably would be faster if i could do it in place but strings are immutable so we have to
+        # recreate the string
+        vowels = {"a", "e", "i", "o", "u", "A", "E", "I", "O", "U"}
+        l = 0
+        r = len(s)-1
+        leftStr = ""
+        rightStr = ""
+
+        while l < r:
+            if s[l] not in vowels:
+                leftStr += s[l]
+                l += 1
+                continue
+            
+            if s[r] not in vowels:
+                rightStr = s[r] + rightStr
+                r -= 1
+                continue
+
+            rightStr = s[l] + rightStr
+            leftStr += s[r]
+            
+            r -= 1
+            l += 1
+
+        if l == r:
+                leftStr += s[l]
+
+        vowelsReversed = leftStr + rightStr
+
+        return vowelsReversed
+    
         # create a set of all vowels.
         # read through string and pick out all chars that are vowels
         # creating a new string of just the vowels
         # reverse vowel string
         # read through string again and instead of recording vowels overwrite them with reversed vowl string
-
-
-        # v2 keep two pointers and start at the start and end of the string
-        # move them each in until they encounter a vowel.
-        # if leftindex < rightindex swap the chars
+        reversedVowels = ""
+        v = []
+        vowels = {"a", "e", "i", "o", "u", "A", "E", "I", "O", "U"}
+        for i in s:
+            if i in vowels:
+                v.append(i)
         
-        # both solutions are O(n) but the second should be moderately faster
+        for i in s:
+            if i in vowels:
+                reversedVowels += v.pop()
+            else:
+                reversedVowels += i
 
         return reversedVowels
 
