5f9390b01072a79b9a18f1fbb6e405ea8ec58f2b
Spencer Fisher <spencer.fisher@billgo.com>
2024-05-09 15:05:01 -0700

    Update 443.string-compression.py
    top 0.03% neat

--- 443.string-compression.py
+++ 443.string-compression.py
@@ -1,3 +1,8 @@
+# @before-stub-for-debug-begin
+from python3problem443 import *
+from typing import *
+# @before-stub-for-debug-end
+
 #
 # @lc app=leetcode id=443 lang=python3
 #
@@ -7,6 +12,34 @@
 # @lc code=start
 class Solution:
     def compress(self, chars: List[str]) -> int:
+        index = 0
+        lastChar = chars[0]
+        lastCharIndex = 0
+        for i in range(len(chars)+1):
+            if i < len(chars) and chars[i] == lastChar:
+                continue
+
+            # new char found.
+            l = i - lastCharIndex
+            chars[index] = lastChar
+            index += 1
+            if l > 1:
+                for c in str(l):
+                    chars[index] = c
+                    index += 1
+
+            if i < len(chars):
+            # reset index/last char
+                lastChar = chars[i]
+                lastCharIndex = i
+        
+        return index
+
+            
+
+
+
+
         
 # @lc code=end
 
