dce7c9eea7c01f2fdd61b09e388e2c2070420439
Spencer Fisher <spencer.fisher@billgo.com>
2024-08-09 15:23:31 -0700

    double reverse method

--- 189.rotate-array.py
+++ 189.rotate-array.py
@@ -12,6 +12,35 @@ from typing import *
 # @lc code=start
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
+        # double reverse method (no human thinks of this)
+
+        l = len(nums)
+        if k >= l:
+            k = k % l
+        if k == 0:
+            return
+        
+        nums.reverse()
+        
+        i = 0
+        while i < l:
+            # if we're in k then swap i and k-i
+            # else swap i and l+k-i
+            swap = k - i - 1
+            if i >= k:
+                swap = l + k - i - 1
+            if swap <= i:
+                if swap >= k:
+                    break
+                i = k
+                continue
+            t = nums[i]
+            nums[i] = nums[swap]
+            nums[swap] = t
+            i += 1
+        return
+
+        # my method
         l = len(nums)
         if k >= l:
             k = k % l
