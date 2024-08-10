0bfd28f4eeb2f7d7ed936861a92e7d578397d451
Spencer Fisher <spencer.fisher@billgo.com>
2024-08-09 18:48:59 -0700

    rust problem solution

--- c# leetcode/obj/Debug/net7.0/c# leetcode.AssemblyInfo.cs
+++ c# leetcode/obj/Debug/net7.0/c# leetcode.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("c# leetcode")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa9567979dbd5733b2814f43f5515dc7112be6a9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25944da3c53efda94182e009887728a53c18568b")]
 [assembly: System.Reflection.AssemblyProductAttribute("c# leetcode")]
 [assembly: System.Reflection.AssemblyTitleAttribute("c# leetcode")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
