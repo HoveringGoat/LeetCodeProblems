48d27feb18d7a288686619b44e4003aa3cd418f6
Spencer Fisher <spencer.fisher@billgo.com>
2024-08-12 18:19:13 -0700

    update

--- rust/src/bin/13.roman-to-integer.rs
+++ rust/src/bin/13.roman-to-integer.rs
@@ -11,70 +11,51 @@ impl Solution {
         // create map for converting digits to chars
         // break down numbers into groups of symbols
         // if we have 4 of a symbol use the larger symbol with current preceding it IX instead of VIIII
-        let mut map: HashMap<i32, char> = HashMap::new();
-        map.insert(1000,'M');
-        map.insert(500,'D');
-        map.insert(100,'C');
-        map.insert(50,'L');
-        map.insert(10,'X');
-        map.insert(5,'V');
-        map.insert(1,'V');
-        let num = 0;
+        let mut map: HashMap<char, i32> = HashMap::new();
+        map.insert('M', 1000);
+        map.insert('D', 500);
+        map.insert('C', 100);
+        map.insert('L', 50);
+        map.insert('X', 10);
+        map.insert('V', 5);
+        map.insert('I', 1);
+        let mut num = 0;
+        let mut current_chunk = 0;
+        let mut last_numeral = ' ';
+        let mut last_chunk = 0;
 
-        for (index, value) in s.iter().enumerate(){
-            println!("index: {index}, value: {value}");
-        }
+        for i in s.chars() {
+            if i == last_numeral{
+                current_chunk += map.get(&i).expect("failed to get value");
+                continue
+            }
+            
+            last_numeral = i;
 
-        num
-    }
-
-    pub fn roman_to_int(s: String) -> String {
-        // create map for converting digits to chars
-        // break down numbers into groups of symbols
-        // if we have 4 of a symbol use the larger symbol with current preceding it IX instead of VIIII
-        let mut map: HashMap<i32, char> = HashMap::new();
-        map.insert(1000,'M');
-        map.insert(500,'D');
-        map.insert(100,'C');
-        map.insert(50,'L');
-        map.insert(10,'X');
-        map.insert(5,'V');
-        map.insert(1,'V');
-        
-        let sizes = [1000, 500, 100, 50, 10, 5, 1]; 
-        let mut remaining_value: i32 = s.parse().expect("invalid input");
-        let mut size_index = 0;
-        let mut numerals = String::new();
-
-        while remaining_value > 0 {
-            let size = sizes[size_index];
-            if remaining_value >= size {
-                let i =  remaining_value/ sizes[size_index];
-                let c = map.get(&size).expect("error getting numeral from map");
-                if i == 4 && size_index > 0{
-                    // add one of current and one of +1 size to numeral string
-                    numerals.push(c);
-                    let d = map.get(&sizes[size_index-1]).expect("error getting numeral from map");
-                    numerals.push(d); // larger numeral
-                }
-                else {
-                    for j in ..i{
-                        numerals.push(c);
-                    }
-                }
-
-                // reduce remaining value by i*size
-                remaining_value -= i*sizes[size_index];
-                size_index += 1;
+            // found start of new chunk
+            if last_chunk > current_chunk{
+                // normal operation add last chunk to num
+                num += last_chunk
             }
             else{
-                // last one doesnt fit increase and try next
-                size_index += 1;
-                continue;
+                // subtraction
+                current_chunk -= last_chunk
             }
+            
+            last_chunk = current_chunk;
+            current_chunk = *map.get(&i).expect("failed to get value");
         }
 
-        numerals
+        if last_chunk > current_chunk{
+            // normal operation add last chunk to num
+            num += last_chunk
+        }
+        else{
+            // subtraction
+            current_chunk -= last_chunk
+        }
+
+        num + current_chunk
     }
 }
 // @lc code=end
