8b6a31d1bbab227b96c7f42742cb2c97a43df0c7
Spencer Fisher <spencer.fisher@billgo.com>
2025-01-06 16:10:55 -0800

    update list solution

--- rust/src/bin/1046.last-stone-weight.rs
+++ rust/src/bin/1046.last-stone-weight.rs
@@ -10,9 +10,33 @@ use std::collections::BinaryHeap;
 
 impl Solution {
     pub fn last_stone_weight(stones: Vec<i32>) -> i32 {
+        let mut sorted_stones: Vec<i32> = stones;
+        sorted_stones.sort();
+        sorted_stones.reverse();
+        while (sorted_stones.len() > 1) {
+            for i in 0..sorted_stones.len(){
+                sorted_stones.sort();
+                sorted_stones.reverse();
+            }
+            let remainder: i32 = sorted_stones.remove(0) - sorted_stones.remove(0);
+            
+            if (remainder > 0){
+                sorted_stones.push(remainder);
+                sorted_stones.sort();
+                sorted_stones.reverse();
+            }
+        }
 
-        let mut 
-        stone_heap: BinaryHeap<i32> = BinaryHeap::from(stones);
+        if (sorted_stones.len() == 1) {
+            return sorted_stones[0];
+        }
+
+        return 0;
+    }
+    
+    pub fn last_stone_weight_heap(stones: Vec<i32>) -> i32 {
+
+        let mut stone_heap: BinaryHeap<i32> = BinaryHeap::from(stones);
 
         while (stone_heap.len() > 1) {
             let remainder: i32 = stone_heap.pop().unwrap_or(0) - stone_heap.pop().unwrap_or(0);
