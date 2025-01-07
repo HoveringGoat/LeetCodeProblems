3d145fedd071d83bd237071ed369bc140d81b392
Spencer Fisher <spencer.fisher@billgo.com>
2025-01-06 17:19:21 -0800

    Update 169.majority-element.rs

--- rust/src/bin/169.majority-element.rs
+++ rust/src/bin/169.majority-element.rs
@@ -9,15 +9,20 @@ use std::collections::HashMap;
 impl Solution {
     pub fn majority_element(nums: Vec<i32>) -> i32 {
         let mut rust_map: HashMap<i32, i32> = HashMap::new();
+        let threshold = (nums.len() as i32) /2;
 
         for i in nums {
-            *(rust_map.entry(i).or_insert(1)) += 1;
+            *(rust_map.entry(i).or_insert(0)) += 1;
+            let v = *rust_map.get(&i).unwrap();
+            if (v > threshold) {
+                return i;
+            }
         }
 
         let mut max = 0;
         let mut majority_element = 0;
 
-        for k in rust_map.keys(){
+        for k in rust_map.keys() {
             let v = *rust_map.get(k).unwrap();
             if (v > max){
                 max = v;
