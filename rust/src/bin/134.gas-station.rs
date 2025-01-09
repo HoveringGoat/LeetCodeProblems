509d47aa331a62d4c67d348a08988d7f8e412c30
Spencer Fisher <spencer.fisher@billgo.com>
2025-01-09 15:27:56 -0800

    Update 134.gas-station.rs

--- rust/src/bin/134.gas-station.rs
+++ rust/src/bin/134.gas-station.rs
@@ -34,6 +34,32 @@ impl Solution {
         return min_station as i32;
         
     }
+    pub fn can_complete_circuit_mymethod(gas: Vec<i32>, cost: Vec<i32>) -> i32 {
+        let mut min = 100;
+        let mut min_station = 0;
+        let mut current_gas = 0;
+        let num_stations = gas.len();
+        for i in 0..num_stations{
+            // add gas
+            // minus cost to next station
+            // record current gas level for station i+1
+            current_gas += gas[i];
+            current_gas -= cost[i];
+
+            if (current_gas < min){
+                min = current_gas;
+                min_station = (i + 1) % num_stations;
+            }
+        }
+
+        // we dont have enough gas to do the circuit
+        if (current_gas < 0) {
+            return -1;
+        }
+
+        return min_station as i32;
+        
+    }
 }
 // @lc code=end
 
